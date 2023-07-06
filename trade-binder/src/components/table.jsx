1b423bbdd3be203c62e1d9944af669e0ffc73bec
GWorsnop <georgeworsnop@gmail.com>
2023-07-06 20:47:55 +0100

    add optimisitic rendering to trade binder

--- trade-binder/src/components/table.jsx
+++ trade-binder/src/components/table.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import changeQuantity from "./api-interaction/changeQuantity";
 import deleteCard from "./api-interaction/deleteCard";
 
 function Table(props) {
   const { cards, setIsDeleted } = props;
+  console.log(cards);
+
+  const [displayCards, setDisplayCards] = useState(cards);
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    setDisplayCards(cards);
+    setEdit(false);
+  }, [cards, edit]);
 
   return (
     <div className="flex flex-row justify-center border-r-2 w-screen">
@@ -10,7 +20,7 @@ function Table(props) {
       <table>
         <tbody>
           <tr>
-            {cards.map((item) => {
+            {displayCards.map((item, index) => {
               return (
                 <td
                   key={item.item_id}
@@ -31,6 +41,8 @@ function Table(props) {
                       className="btn bg-green-400 hover:bg-green-500 text-xs"
                       onClick={(e) => {
                         e.preventDefault();
+                        item.quantity++;
+                        setEdit(true);
                         changeQuantity(1, item.card_id);
                       }}
                     >
@@ -41,9 +53,13 @@ function Table(props) {
                       onClick={(e) => {
                         if (item.quantity > 1) {
                           e.preventDefault();
+                          item.quantity--;
+                          setEdit(true);
                           changeQuantity(-1, item.card_id);
                         } else {
                           e.preventDefault();
+                          item.quantity--;
+                          setEdit(true);
                           setIsDeleted(true);
                           deleteCard(item.card_id);
                         }
